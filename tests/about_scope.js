0a7f8b0527dab5532b48c1518654528dcb5c57a4
Ignacio Loureiro <ignacio.loureiro@endava.com>
2020-06-05 17:43:49 -0300

    scope

--- tests/about_scope.js
+++ tests/about_scope.js
@@ -1,24 +1,25 @@
-describe("About Scope (topics/about_scope.js)", ()=>{
+describe("About Scope (topics/about_scope.js)", () => {
+  thisIsAGlobalVariable = 77;
 
-thisIsAGlobalVariable = 77;
+  test("global variables", function () {
+    equal(
+      __,
+      thisIsAGlobalVariable,
+      "is thisIsAGlobalVariable defined in this scope?"
+    );
+  });
 
-test("global variables", function() {
-    equal(__, thisIsAGlobalVariable, 'is thisIsAGlobalVariable defined in this scope?');
-});
-
-test("variables declared inside of a function", function() {
+  test("variables declared inside of a function", function () {
     let outerVariable = "outer";
 
     // this is a self-invoking function. Notice that it calls itself at the end ().
-    (function() {
-        let innerVariable = "inner";
-        expect(__, outerVariable, 'is outerVariable defined in this scope?');
-        expect(__, innerVariable, 'is innerVariable defined in this scope?');
+    (function () {
+      let innerVariable = "inner";
+      expect(__, outerVariable, "is outerVariable defined in this scope?");
+      expect(__, innerVariable, "is innerVariable defined in this scope?");
     })();
 
-    equal(__, outerVariable, 'is outerVariable defined in this scope?');
-    equal(__, typeof(innerVariable), 'is innerVariable defined in this scope?');
+    equal(__, outerVariable, "is outerVariable defined in this scope?");
+    equal(__, typeof innerVariable, "is innerVariable defined in this scope?");
+  });
 });
-
-});
-
